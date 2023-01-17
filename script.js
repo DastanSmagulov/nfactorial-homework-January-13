42d8db7fbf3e52b8b7651141704078ec6e383f12
Dastan <73186335+DastanSmagulov@users.noreply.github.com>
2023-01-17 17:31:37 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+// first
+// const user = {};
+
+// let name = prompt("Your name: ");
+
+// let age = prompt("Your age: ");
+
+// let sex = prompt("What's your sex?");
+
+// user.name = name;
+// user.age = age;
+// user.sex = sex;
+
+// console.log(user);
+
+// second 
+// let number = parseInt(prompt("Enter number"));
+
+// if (isNaN(number)) {
+//   alert("It is not a number");
+// } else {
+//     console.log(number)
+//   alert(`Your number is ${number}`);
+// }
+
+// third
+
+// let a = +prompt("a?", "");
+// console.log(a); // string
+// a = +a;
+// console.log(a); // number
+
+// switch(a){
+//     case 0:
+//         alert(0)
+//         break
+//     case 1:
+//         alert(1)
+//         break
+//     case 2 || 3: 
+//         alert("2,3");
+//         break
+// }
+
+// fourth
+// let sum = 0;
+
+// for(let i = 0; i < 100; i++){
+//     if(i % 2 == 0){
+//         sum+= i;
+//     }
+// }
+
+// let i = 0;
+// while(i < 3){
+//     alert(`number ${i}!`);
+//     i++;
+// }
